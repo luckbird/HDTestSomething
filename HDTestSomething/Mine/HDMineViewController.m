31be0a223e547f9a5709ab1e571883458ca5cd60
yscompany <814880548@qq.com>
2017-06-05 19:07:49 +0800

    Add 跑马灯效果文字展示

--- HDTestSomething/Mine/HDMineViewController.m
+++ HDTestSomething/Mine/HDMineViewController.m
@@ -9,7 +9,9 @@
 #import "HDMineViewController.h"
 
 @interface HDMineViewController ()
-
+@property (nonatomic, strong) UILabel *currentLabel;
+@property (nonatomic, strong) NSTimer *time;
+@property (nonatomic, strong) CADisplayLink *adtimer;
 @end
 
 @implementation HDMineViewController
@@ -17,9 +19,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Do any additional setup after loading the view.
+    [self.view addSubview:self.currentLabel];
+    
+  
+        // Do any additional setup after loading the view.
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    self.adtimer.paused = NO;
+}
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    self.adtimer.paused = YES;
 }
 
+#pragma mark - action method
+- (void)adStartAnimating {
+    CGFloat distance = kScreenW / 600;
+    CGFloat adLabelX = _currentLabel.frame.origin.x - distance;
+    CGRect tempF = _currentLabel.frame;
+    if (adLabelX < -(_currentLabel.frame.size.width)) {
+        tempF.origin.x = kScreenW ;
+        _currentLabel.frame = tempF;
+        return;
+    }
+    tempF.origin.x = adLabelX;
+    _currentLabel.frame = tempF;
+}
+
+#pragma mark -  getter method
+- (UILabel *)currentLabel {
+    if (!_currentLabel) {
+        _currentLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, kScreenW, 20)];
+        _currentLabel.backgroundColor = [UIColor clearColor];
+        _currentLabel.text = @"我就是用来测试跑马灯文字展示的";
+        CGRect frame = _currentLabel.frame;
+        CGSize dims = [_currentLabel.text sizeWithFont:self.currentLabel.font];
+        frame.origin.x = dims.width >kScreenW ? dims.width:kScreenW;      //设置起点
+        frame.size.width = dims.width;
+        _currentLabel.frame = frame;
+        _currentLabel.textColor = [UIColor redColor];
+        _currentLabel.font = [UIFont systemFontOfSize:16.0f];
+    }
+    return _currentLabel;
+}
+- (CADisplayLink *)adtimer {
+    if (!_adtimer) {
+        _adtimer = [CADisplayLink displayLinkWithTarget:self selector:@selector(adStartAnimating)];
+        [_adtimer addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    }
+    return _adtimer;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
